38c580f4199d30644bc57666a0aa78bba419f05d
Yale Liu <hiyale@vip.163.com>
2016-11-23 10:32:51 +0800

    group & user shema added

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,1 +1,18 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+    {
+        username:{ type:String, required:true, index: true},
+        password: {type:String, required:true},
+        email:{ type:String, required:true, unique: true},
+        name: {
+            firstname:String,
+            lastname:String
+        },
+        avator: String,
+        usergroup: String,
+        create_date:{ type: Date, default: Date.now},
+        update_date:{ type: Date, default: Date.now}
+    }
+);
