e0f7d35c570629fc20e2b23e4de4b13b646d316d
hiyale <hiyale@vip.163.com>
2016-12-08 16:11:39 +0800

    user-clud

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-router.get('', function(req, res){
+router.get('/', function(req, res){
     User.getAllUsers(function(err, users){
         if(err){
             throw err;
@@ -12,13 +12,43 @@ router.get('', function(req, res){
     })
 }),
 
-router.get('/login', function(req, res){
-    res.end('please login');
-});
+router.get('/:_id', function(req, res){
+    var id = req.params._id;
+    User.getUser(id, function(err, user){
+        if(err){
+            throw err;
+        }
+        res.setHeader("Access-Control-Allow-Origin","*");
+        res.json(user);
+    });
+}),
+
+router.put('/:_id', function(req, res){
+    var id = req.params._id;
+    var user = req.body;
+    User.updateUser(id, user, {}, function(err, user){
+        if(err){
+            throw err;
+        }
+        res.setHeader("Access-Control-Allow-Origin","*");
+        res.json(user);
+    })
+})
 
 router.post('/', function(req, res){
     var user = req.body;
     User.addUser(user, function(err, user){
+        if(err){
+            throw err;
+        }
+        res.setHeader("Access-Control-Allow-Origin","*");
+        res.json(user);
+    })
+});
+
+router.delete('/:_id', function(req, res){
+    var id = req.params._id;
+    User.deleteUser(id, function(err, user){
         if(err){
             throw err;
         }
