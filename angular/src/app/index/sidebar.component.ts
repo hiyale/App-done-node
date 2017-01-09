40b56eeb80e4a1f141258f0b49b2401360ce9f6f
hiyale <hiyale@vip.163.com>
2017-01-09 15:58:14 +0800

    sidebar-html

--- angular/src/app/index/sidebar.component.ts
+++ angular/src/app/index/sidebar.component.ts
@@ -2,18 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'sidebar',
-    template: `
-<md-sidenav-container class="sidenav-container">
-    <md-sidenav #end align="end">
-        <div style="width:300px">
-                hello alladdaff  safddfas
-        </div>
-        <button md-button (click)="end.close()">Close</button>
-    </md-sidenav>
-
-<button md-button (click)="end.open()" style="right:0; top:0; position:fixed">Open end sidenav</button>
-
-</md-sidenav-container>`,
+    templateUrl: 'sidebar.component.html' ,
     styleUrls: ['./index.css']
 })
 export class SidebarComponent implements OnInit {
